8f17ad7dbe7cbf4395fa6f3826a39771927d7870
Andrej Pavlovic <andrej.pavlovic@massedge.com>
2023-11-09 21:34:14 -0500

    defined `@blueconic/blueconic-react-native` exports

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,132 @@
-import { NativeModulesProxy, EventEmitter, Subscription } from 'expo-modules-core';
+import BlueConicRN from '@blueconic/blueconic-react-native'
+export default BlueConicRN
 
-// Import the native module. On web, it will be resolved to ExpoBlueConic.web.ts
-// and on native platforms to ExpoBlueConic.ts
-import ExpoBlueConicModule from './ExpoBlueConicModule';
-import ExpoBlueConicView from './ExpoBlueConicView';
-import { ChangeEventPayload, ExpoBlueConicViewProps } from './ExpoBlueConic.types';
+/**
+ * Returns the ID of the BlueConic Profile.
+ * @returns The ID of the BlueConic profile
+ */
+export const getProfileId = async (): Promise<string> => BlueConicRN.getProfileId()
 
-// Get the native constant value.
-export const PI = ExpoBlueConicModule.PI;
+/**
+ * Returns the first value for a given profile property.
+ * @param property The profile property to get the value for.
+ * @returns The first value
+ */
+export const getProfileValue = async (property: string): Promise<string> => BlueConicRN.getProfileValue(property)
 
-export function hello(): string {
-  return ExpoBlueConicModule.hello();
+/**
+ * Return the values for a given profile property.
+ * @param property The profile property to get the values for.
+ * @returns A collection containing the values.
+ */
+export const getProfileValues = async (property: string): Promise<string[]> => BlueConicRN.getProfileValues(property)
+
+
+/**
+ * Adds a single property value to the profile. If there are already values for a property the new value will be added. Values for a property need to be unique; passing the same value multiple times will have no effect.
+ * @param property The profile property to add the values for.
+ * @param value The property value to add to the profile.
+ */
+export const addProfileValue = (property: string, value: string) => {
+  BlueConicRN.addProfileValue(property, value)
 }
 
-export async function setValueAsync(value: string) {
-  return await ExpoBlueConicModule.setValueAsync(value);
+/**
+ * Adds property values to the profile. The values from the collection are added to the profile. If there are already values for a property the new values will be added. Values for a property need to be unique; passing the same values multiple times will have no effect.
+ * @param property The profile property to add the values for.
+ * @param values The property values to add to the profile.
+ */
+export const addProfileValues = (property: string, values: string[]) => {
+  BlueConicRN.addProfileValues(property, values)
 }
 
-const emitter = new EventEmitter(ExpoBlueConicModule ?? NativeModulesProxy.ExpoBlueConic);
-
-export function addChangeListener(listener: (event: ChangeEventPayload) => void): Subscription {
-  return emitter.addListener<ChangeEventPayload>('onChange', listener);
+/**
+ * It sets value on the profile. Passing a property that was already set with value will cause the old values to be removed.
+ * @param name The profile property to add the values for.
+ * @param value The profile values to store.
+ */
+export const setProfileValue = (name: string, value: string) => {
+  BlueConicRN.setProfileValue(name, value)
 }
 
-export { ExpoBlueConicView, ExpoBlueConicViewProps, ChangeEventPayload };
+/**
+ * It sets values on the profile. Passing a property that was already set with values will cause the old values to be removed.
+ * @param name The profile property to add the values for.
+ * @param values The profile values to store.
+ */
+export const setProfileValues = (name: string, values: string[]) => {
+  BlueConicRN.setProfileValues(name, values)
+}
+
+/**
+ * Setter for the locale to get the parameters for. By default, the default locale configured in BlueConic is used.
+ * Note: the only valid locales currently are `'en_US'` and `'nl_NL'`.
+ * @param locale The locale, e.g.‘en_US’.
+ */
+export const setLocale = (locale: 'en_US' | 'nl_NL') => {
+  BlueConicRN.setLocale(locale)
+}
+
+/**
+ * Registers an event of the specified type with the given properties. For a `“PAGEVIEW"` event a screen name can be passed, so interactions can be restricted on the where tab in BlueConic. For a `“VIEW"`, `“CLICK"` or `“CONVERSION"` event an interactionId should be passed to register the event for.
+ * @param eventType The event type. (e.g:`"PAGEVIEW"`,`"VIEW"`,`“CLICK"`,`“CONVERSION"`)
+ * @param properties A map with properties for the event.
+ */
+export function createEvent(eventType: 'PAGEVIEW', properties: {screenName: string})
+export function createEvent(eventType: 'VIEW' | 'CLICK' | 'CONVERSION', properties: {interactionId: string})
+export function createEvent(eventType: 'PAGEVIEW' | 'VIEW' | 'CLICK' | 'CONVERSION', properties: {screenName: string} | {interactionId: string}) {
+  BlueConicRN.createEvent(eventType, properties)
+}
+
+
+/**
+ * Registers a page event. For a “PAGEVIEW" event a screen name can be passed, so interactions can be restricted on the where tab in BlueConic.
+ * @param screenName The screen name for this page view
+ */
+export const registerPageView = (screenName: string) => {
+  BlueConicRN.registerPageView(screenName)
+}
+
+/**
+ * Registers a dialogue event of the specified type with the given properties. For a “VIEW", “CLICK" or “CONVERSION" event an interactionId should be passed to register the event for.
+ * @param eventType The event type. (e.g:“VIEW",“CLICK",“CONVERSION")
+ * @param interactionId The interaction id.
+ */
+export const registerDialogueEvent = (eventType: 'VIEW' | 'CLICK' | 'CONVERSION', interactionId: string) => {
+  BlueConicRN.registerDialogueEvent(eventType, interactionId)
+}
+
+/**
+ * Register a user click. For the event, a selector can be passed to trigger BlueConic Listeners, to execute their logic.
+ * @param selector The selector, which can be used in the listener rules, to listen on
+ */
+export const registerClickEvent = (selector: string) => {
+  BlueConicRN.registerClickEvent(selector)
+}
+
+/**
+ * Register a user click. For the event, a selector can be passed to trigger BlueConic Listeners, to execute their logic.
+ * @param selector The selector, which can be used in the listener rules, to listen on
+ * @param values context of the click event 
+ */
+export const registerClickEventWithContext = (selector: string, values: string[]) => {
+  BlueConicRN.registerClickEventWithContext(selector, values)
+}
+
+/**
+ * Register a user update values event. For the event, a selector can be passed to trigger BlueConic Listeners, to execute their logic.
+ * @param selector The selector, which can be used in the listener rules, to listen on
+ * @param values context of the update values event
+ */
+export const registerUpdateValuesEvent = (selector: string, values: string[]) => {
+  BlueConicRN.registerUpdateValuesEvent(selector, values)
+}
+
+/**
+ * Register a user update values event. For the event, a selector can be passed to trigger BlueConic Listeners, to execute their logic.
+ * @param name The name of the advanced even, which can be used in the listener rules, to listen on
+ * @param values context of the advanced event
+ */
+export const registerAdvancedEvent = (name: string, values: string[]) => {
+  BlueConicRN.registerAdvancedEvent(name, values)
+}
